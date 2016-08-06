959d35b1dc4ed82f310a2eaac2871a77a8df66d3
Cass-D <1790887746@qq.com>
2016-08-06 22:27:19 +0800

    仿外卖
    仿外卖APP

---
+++ .cache/mapActivity/mapActivity.w.view__zh_CN_Mobile_.js
@@ -0,0 +1,21 @@
+window.__justep.__ResourceEngine.loadCss([{url: '/v_c9b08b5538da4f7a83998bd5dfa09a47l_zh_CNs_d_m/system/components/comp.min.css', include: '$model/system/components/justep/row/css/row,$model/system/components/justep/attachment/css/attachment,$model/system/components/justep/barcode/css/barcodeImage,$model/system/components/justep/panel/css/panel,$model/system/components/justep/common/css/scrollable,$model/system/components/justep/scrollView/css/scrollView,$model/system/components/justep/input/css/datePickerPC,$model/system/components/justep/contents/css/contents,$model/system/components/justep/popMenu/css/popMenu,$model/system/components/justep/lib/css/icons,$model/system/components/justep/titleBar/css/titleBar,$model/system/components/justep/dataTables/css/dataTables,$model/system/components/justep/dialog/css/dialog,$model/system/components/justep/messageDialog/css/messageDialog,$model/system/components/justep/toolBar/css/toolBar,$model/system/components/justep/popOver/css/popOver,$model/system/components/justep/loadingBar/loadingBar,$model/system/components/justep/input/css/datePicker,$model/system/components/justep/dataTables/css/dataTables,$model/system/components/justep/wing/css/wing,$model/system/components/bootstrap/scrollSpy/css/scrollSpy,$model/system/components/justep/menu/css/menu,$model/system/components/justep/numberSelect/css/numberList,$model/system/components/justep/list/css/list,$model/system/components/bootstrap/carousel/css/carousel,$model/system/components/bootstrap/dropdown/css/dropdown,$model/system/components/justep/common/css/forms,$model/system/components/justep/bar/css/bar'},{url: '/v_9337272ca6bb4fbb9c0d6a96e5b41c0bl_zh_CNs_d_m/system/components/bootstrap.min.css', include: '$model/system/components/bootstrap/lib/css/bootstrap,$model/system/components/bootstrap/lib/css/bootstrap-theme'}]);window.__justep.__ResourceEngine.loadJs(['/v_483e5335a2794ceaab1c29fbedce8789l_zh_CNs_d_m/system/components/comp.min.js','/v_f542b8e8c173429e9cf6355da56fcb53l_zh_CNs_d_m/system/common.min.js','/v_3f46513c826a4a1891d45621a3f1a463l_zh_CNs_d_m/system/core.min.js']);define(function(require){
+require('$model/UI2/system/components/justep/windowReceiver/windowReceiver');
+require('$model/UI2/system/components/justep/loadingBar/loadingBar');
+require('$model/UI2/system/components/justep/panel/panel');
+require('$model/UI2/system/components/justep/panel/child');
+require('$model/UI2/system/components/justep/model/model');
+require('$model/UI2/system/components/justep/window/window');
+require('$model/UI2/system/components/justep/titleBar/titleBar');
+require('$model/UI2/system/components/justep/button/button');
+var __parent1=require('$model/UI2/system/lib/base/modelBase'); 
+var __parent0=require('$model/UI2/takeout/mapActivity'); 
+var __result = __parent1._extend(__parent0).extend({
+	constructor:function(contextUrl){
+	this.__sysParam='true';
+	this.__contextUrl=contextUrl;
+	this.__id='';
+	this.__cid='cuMVryu';
+	this._flag_='9ba4a00ced7eee0b0c3419739e751dae';
+	this.callParent(contextUrl);
+}}); 
+return __result;});
